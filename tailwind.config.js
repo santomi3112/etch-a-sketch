e20fb0765f67641caf8f216749f0853e37f5e100
santomi3112 <adiprasetio007@gmail.com>
2022-10-13 09:00:57 +0700

    added space color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       fontFamily: {
         'silkscreen': ['Silkscreen', 'cursive'],
       },
+      colors: {
+        'spaceCadet': '#39375B',
+        'biru2': '#14213D',
+        'kuning3': '#FCA311',
+        'abu4': '#E5E5E5'
+      },
     },
   },
   plugins: [],
