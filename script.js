d9058390413fcf4414fb83726e9e342cd8676162
santomi3112 <adiprasetio007@gmail.com>
2022-10-13 15:17:39 +0700

    added grid default value

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const grid = document.querySelector("[data-grid]");
+const GRID = document.querySelector("[data-grid]");
 const slider = document.querySelector("[data-slider]");
 const screenValue = document.querySelector("[data-slider-value]");
 
@@ -8,3 +8,20 @@ slider.addEventListener("input", () => {
   let val = document.querySelector("[data-slider]").value;
   screenValue.textContent = val;
 });
+
+// create default grid value
+function createDefaultGrid() {
+  // 16 x 16 = 256 grid value
+  for (let i = 0; i < 256; i++) {
+    // create div element
+    const div = document.createElement("div");
+    // adding class to div
+    div.classList.add("border");
+    div.classList.add("border-black");
+    div.classList.add("bg-white");
+    // add child to GRID
+    GRID.appendChild(div);
+  }
+}
+
+createDefaultGrid();
